9d1d14da3e1653180f7f1543ea218e307fd90619
Sai Byri <saikiran169@gmail.com>
2022-01-18 13:58:59 -0800

    added utils.js

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db/models')
+const bcrypt = require('bcryptjs')
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* GET user sing up page */
+router.get('/sign-up', )
 
 module.exports = router;
