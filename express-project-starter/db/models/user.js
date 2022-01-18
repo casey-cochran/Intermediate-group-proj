120e0398a0958c30cf05feb90ea105ab9e02c9d6
Jacob Holdowsky <jacobedward1995@gmail.com>
2022-01-18 16:36:32 -0500

    model updated

--- express-project-starter/db/models/user.js
+++ express-project-starter/db/models/user.js
@@ -1,9 +1,19 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    fullname: DataTypes.STRING,
-    email: DataTypes.STRING,
-    hashedPass: DataTypes.STRING
+    fullname: {
+      allowNull: false,
+      type: DataTypes.STRING(100),
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING(50),
+      unique: true
+    },
+    hashedPass: {
+      allowNull: false,
+      type: DataTypes.STRING(255)
+    }
   }, {});
   User.associate = function(models) {
     // associations can be defined here
