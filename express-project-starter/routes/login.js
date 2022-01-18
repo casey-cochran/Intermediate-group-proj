d05b79847092f696674203a7f60a42a40b96f1ff
Bryan <bryanscottarnold@gmail.com>
2022-01-18 17:35:44 -0500

    updated login and added authjs

--- express-project-starter/routes/login.js
+++ express-project-starter/routes/login.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db/models')
+const bcrypt = require('bcryptjs')
+const { userLogin, userLogout } = require('../auth.js');
+
+const { asyncHandler, csrfProtection } = require("./utils");
+
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+    res.render('login', {
+        title: 'Login',
+        csrfToken: req.csrfToken(),
+    });
 });
+
+
+
 
 module.exports = router;
