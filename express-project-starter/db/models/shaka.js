b89ffdac04b25aa2cd2a95cd6aff473a86ed2c7c
Jacob Holdowsky <jacobedward1995@gmail.com>
2022-01-19 13:01:55 -0500

    made all migrations and models

---
+++ express-project-starter/db/models/shaka.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Shaka = sequelize.define('Shaka', {
+    hobbyPostId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {});
+  Shaka.associate = function(models) {
+    // associations can be defined here
+    Shaka.belongsTo(models.User, {foreignKey: 'userId'})
+  };
+  return Shaka;
+};
--- express-project-starter/db/models/user.js
+++ express-project-starter/db/models/user.js
@@ -19,8 +19,24 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING.BINARY
     }
   }, {});
-  User.associate = function(models) {
+  User.associate = function (models) {
     // associations can be defined here
+    User.hasMany(models.HobbyPost, { foreignKey: 'userId' })
+    User.hasMany(models.Shaka, {foreignKey: 'userId'})
+    const columnMappingOne = { // User -> User, through Follow as follower
+      through: 'Follower',
+      otherKey: 'followingId',
+      foreignKey: 'followerId',
+      as: 'followings'
+    }
+    const columnMappingTwo = { // User -> User, through Follow as following
+      through: 'Follower',
+      otherKey: 'followerId',
+      foreignKey: 'followingId',
+      as: 'followers'
+    }
+    User.belongsToMany(models.User, columnMappingOne);
+    User.belongsToMany(models.User, columnMappingTwo);
   };
   return User;
 };
