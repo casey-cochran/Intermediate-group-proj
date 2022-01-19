bf9cdb8c707c6e799bde7ab61f3b4ad31f561239
Jacob Holdowsky <jacobedward1995@gmail.com>
2022-01-19 13:34:28 -0500

    seeded

---
+++ express-project-starter/db/seeders/20220119182708-usersSeeder.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.bulkInsert('Users', [
+      { firstName: 'Jack', lastName: 'Sparrow', email: "demo@demo.com", hashedPass: 'SmittyWerbenjägermanjensen(He was #1)', createdAt: new Date(), updatedAt: new Date() },
+
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkDelete('Users', null, {});
+  }
+};
