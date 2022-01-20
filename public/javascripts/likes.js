f23bd777dd5f2a97f55c3beec973b26d1623e402
Jacob Holdowsky <jacobedward1995@gmail.com>
2022-01-20 17:04:39 -0500

    likes in progress

--- public/javascripts/likes.js
+++ public/javascripts/likes.js
@@ -2,9 +2,10 @@
 
     const likesButton = document.querySelector('.likes');
 
-    let count = 4
+    let count = 0
     likesButton.addEventListener('click', async (e) => {
         // e.preventDefault()
+        // const shakasCount = await db.Shaka.count
         const postId = likesButton.id
         console.log(postId)
         const res = await fetch(`/hobbyposts/${postId}/likes`, {
@@ -12,21 +13,20 @@
         })
 
         const likes = await res.json()
-        console.log('we made it!')
-        const div = document.createElement('div')
-        const p = document.createElement('p')
-        document.body.append(div)
-        p.innerText = ' hello '
-        div.append(p)
-        likesButton.innerText = `likes ${count}`
+        // console.log('we made it!')
+        // const div = document.createElement('div')
+        // const p = document.createElement('p')
+        // document.body.append(div)
+        // div.append(p)
+        likesButton.innerText = `Shaka: ${count}`
 
-        if(likes){
-            count--
-             p.innerText = `likes ${count}`
+        if(likes.status === 'liked'){
+            count++
+            likesButton.innerText = `Shaka'd ${count}`
 
         }else {
-            count++
-            p.innerText = `likes ${count}`
+            count--
+            likesButton.innerText = `Shaka ${count}`
 
         }
        const form = document.querySelector('form')
