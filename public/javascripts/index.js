0122c230fc11bba31f234f022f7b3e219b474d48
Casey Cochran <Casey3362@yahoo.com>
2022-01-20 20:36:20 -0800

    added order in router.get route and fixed comments display

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,26 +1,47 @@
 import { handleErrors } from "./utils.js";
-const commentButton = document.querySelector('.submit-comment');
-const form = document.querySelector('.create-comment-form')
 
-form.addEventListener('submit', async (e) => {
-  e.preventDefault()
-  console.log('are we in ')
-  const postId = commentButton.id
-  const formData = new FormData(form)
-  const content = formData.get('content')
-  const body  = {content};
-  const res = await fetch(`/hobbyposts/${postId}/comments`, {
-    method: "POST",
-    body: JSON.stringify(body),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  });
+document.addEventListener("DOMContentLoaded", async (e) => {
 
-  const comment = await res.json();
-  const commentContainer = document.querySelector('.comments-body')
-  const p = document.createElement('p')
-  p.innerText = `${comment.comment.content}`
-   commentContainer.prepend(p)
 
+
+    const commentButton = document.querySelector('.submit-comment');
+    const form = document.querySelector('.create-comment-form')
+    //const divName = document.querySelector('.user-header')
+    //console.log(divName.id)
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault()
+        console.log('are we in ')
+        const postId = commentButton.id
+        const formData = new FormData(form)
+        const content = formData.get('content')
+        const body = { content };
+        const res = await fetch(`/hobbyposts/${postId}/comments`, {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        const comment = await res.json();
+
+        let commentsBox = document.querySelector('.comments-box')
+
+        const h3 = document.createElement('h3')
+        const p = document.createElement('p')
+
+        h3.innerText = `${comment.user.firstName} ${comment.user.lastName}`
+        p.innerText = `${comment.comment.content}`
+        h3.append(p);
+
+
+
+        commentsBox.prepend(h3)
+        //}
+
+
+
+
+    })
 })
