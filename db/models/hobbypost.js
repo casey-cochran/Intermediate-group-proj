411a7a7fe88524229414f6f6231895f362ec91c1
Sai Byri <saikiran169@gmail.com>
2022-01-21 09:22:20 -0800

    fixed delete assocaition issue

--- db/models/hobbypost.js
+++ db/models/hobbypost.js
@@ -17,8 +17,8 @@ module.exports = (sequelize, DataTypes) => {
   HobbyPost.associate = function (models) {
     // associations can be defined here
     HobbyPost.belongsTo(models.User, { foreignKey: 'userId' })
-    HobbyPost.hasMany(models.Comment, { foreignKey: 'hobbyPostId' })
-    HobbyPost.hasMany(models.Shaka, { foreignKey: 'hobbyPostId' })
+    HobbyPost.hasMany(models.Comment, { foreignKey: 'hobbyPostId', onDelete: 'cascade', hooks:true })
+    HobbyPost.hasMany(models.Shaka, { foreignKey: 'hobbyPostId', onDelete: 'cascade', hooks:true })
   };
   return HobbyPost;
 };
