d7d2c7368735815d0677122f938e49e96b823d37
Sai Byri <saikiran169@gmail.com>
2022-01-23 17:48:56 -0800

    everything is done

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -24,14 +24,17 @@ document.addEventListener("DOMContentLoaded", async (e) => {
         });
 
         const comment = await res.json();
+        console.log(comment)
 
-        let commentsBox = document.querySelector('.comments-box')
+        let commentsBox = document.querySelector('.comments-container')
 
-    
+
         const h3 = document.createElement('h3')
         const p = document.createElement('p')
         const h4 = document.createElement('h4')
         const newDiv = document.createElement('div')
+        const a = document.createElement('a')
+        a.setAttribute("href", `/hobbyposts/${comment.comment.hobbyPostId}/comments/${comment.comment.id}/edit`)
         const options = { month: 'short', day: 'numeric' }
 
         const createdDate = comment.comment.createdAt
@@ -41,9 +44,11 @@ document.addEventListener("DOMContentLoaded", async (e) => {
         h3.innerText = `${comment.user.firstName} ${comment.user.lastName}`
         h4.innerText = `${displayDate}`
         p.innerText = `${comment.comment.content}`
+        a.innerText = `edit`
         newDiv.append(h3)
         newDiv.append(h4)
         newDiv.append(p);
+        newDiv.append(a)
 
 
 
@@ -55,11 +60,46 @@ document.addEventListener("DOMContentLoaded", async (e) => {
         newDiv.style.borderBottom = 'solid 1px rgb(211, 211, 211)'
         p.style.lineHeight= '1.6'
 
-        commentsBox.prepend(newDiv)
-        //}
+        a.style.display= 'flex'
+        a.style.justifyContent= 'flex-end'
+        a.style.fontFamily= 'Helvetica Neue, Helvetica, Arial, sans-serif'
+        a.style.color= 'white'
+        a.style.backgroundColor= 'black'
+        a.style.border= 'solid 1px black'
+        a.style.borderRadius= '50px'
+        a.style.fontWeight= '1px'
+        a.style.padding= '6px 10px'
+        a.style.textAlign= 'center'
+        a.style.textDecoration= 'none'
+        a.style.marginTop= '5px'
+        a.style.marginBottom= '10px'
+        a.style.marginLeft= '600px'
+        a.style.fontSize= '15px'
+
+
+        // .edit-comment-btn{
+            //     display: flex;
+            //     justify-content: flex-end;
+            //     font-family: sohne, Helvetica Neue, Helvetica, Arial, sans-serif;
+            //     /* text-shadow: 0 1px #fff; */
+            //     color: rgb(255, 255, 255);
+            //     font-weight: 1px;
+            //     border: black solid;
+            //     border-radius: 50px;
+            //     padding: 4px 8px;
+            //     text-align: center;
+            //     background-color: rgb(0, 0, 0);
+            //     transition-duration: 00.2s;
+            //     text-decoration: none;
+            //     margin-top: 5px;
+            //     margin-bottom: 10px;
+            //     margin-left: 600px;
+            // }
+            commentsBox.prepend(newDiv)
+            //}
 
 
 
 
-    })
+        })
 })
